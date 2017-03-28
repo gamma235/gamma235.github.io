e1a00ee9d63aae3ffd2631c926db7d0d6c5cb6ab
Jesse <jesseluisdiaz@gmail.com>
2017-03-27 21:27:21 -0500

    first real commit

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,8 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Utils
+import Html.Events exposing (..)
+-- import Css exposing (..)
 
 
 main : Program Never Model Msg
@@ -14,27 +15,30 @@ main =
         , subscriptions = subscriptions
         }
 
-
-
 -- Model
-
-
 type alias Model =
-    { name : String }
-
+    { title : String
+    , menu1 : String
+    , menu2 : String
+    , menu3 : String
+    , menu4 : String
+    , menu5 : String
+    }
 
 init : ( Model, Cmd Msg )
 init =
-    Model "world" ! []
+    Model "テスト　タイトル"
+          "メニュー　一"
+          "メニュー　二"
+          "メニュー　三"
+          "メニュー　四"
+          "メニュー　五" ! []
 
 
 
 -- Update
-
-
 type Msg
     = NoOp
-
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -45,18 +49,96 @@ update msg model =
 
 
 -- View
+type alias Style = List ( String, String )
+
+backdrop : Style
+backdrop =
+    [ ("background", "#fafafa")
+    , ("width", "100%")
+    , ("height", "100%")
+    ]
+
+banner : Style
+banner =
+    [ ("background", "rgb(66, 133, 244)")
+    , ("width", "100%")
+    , ("height", "250px")
+    , ("border-bottom", "lightgray 3px solid")
+    ]
+
+copy : Style
+copy =
+    [ ("font-family", "Roboto,sans-serif")
+    , ("font-weight", "lighter")
+    , ("color", "#fff")
+    ]
+
+title : Style
+title = List.append [ ("padding-top", "90px")
+                    , ("padding-left", "7%")
+                    , ("font-size", "40px")
+                    ] copy
 
 
-view : Model -> Html Msg
+menu : Style
+menu =
+     [ ("background", "#00bcd4")
+     , ("height", "50px")
+     , ("width", "100%")
+     , ("top", "200px")
+     , ("position", "absolute")
+     ]
+
+
+menuItem : Style
+menuItem =
+    List.append [ ("font-size", "16px")
+                , ("display", "inline-block")
+                , ("width", "10%")
+            --    , ("background", "#f6916b")
+                , ("cursor", "pointer")
+                , ("height", "50px")
+                ] copy
+
+menuLabel : Style
+menuLabel =
+    [ ("vertical-align", "middle")
+    , ("padding-left", "20px")
+    ]
+
+view :  Model -> Html Msg
 view model =
-    h1 []
-        [ text ("Hello " ++ (Utils.capitalize model.name)) ]
+  div [ style backdrop ]
+      [ div [ style banner ]
+            [ h1 [ style title ]
+                 [ text model.title ]
 
-
+            , div [style menu]
+                  [ a [ style menuItem ]
+                      [ p [style menuLabel]
+                          [text model.menu1 ]
+                      ]
+                  , a [style menuItem]
+                      [ p [style menuLabel]
+                          [text model.menu2 ]
+                      ]
+                  , a [style menuItem]
+                      [ p [style menuLabel]
+                          [text model.menu3 ]
+                      ]
+                  , a [style menuItem]
+                      [ p [style menuLabel]
+                          [text model.menu4 ]
+                      ]
+                  , a [style menuItem]
+                      [ p [style menuLabel]
+                          [text model.menu5]
+                      ]
+                  ]
+           ]
+      ]
 
 -- Subscriptions
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
